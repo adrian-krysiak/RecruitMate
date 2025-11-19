17d2d93f72e6368898c7a91acd0c1bbe27f985c9
Adrian <adrian.c.krysiak@gmail.com>
2025-11-19 18:48:47 +0100

    Implement custom user model and update settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+
+    # Local apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
